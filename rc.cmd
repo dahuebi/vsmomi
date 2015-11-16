316492e686489d11b1f2446d79fffb12c832613e
Andreas Huber <huber@mst.ch>
2015-11-16 09:06:06 +0100

    Fix rc.sh, rc.cmd

--- rc.cmd
+++ rc.cmd
@@ -3,4 +3,4 @@ python -m virtualenv .
 call Scripts/activate.bat
 pip install -r requirements.txt
 set PYTHONPATH=%CD%
-set PATH=%CD%\bin;%PATH%
+set PATHEXT=.PY;%PATHEXT%
--- rc.sh
+++ rc.sh
@@ -3,4 +3,3 @@ python -m virtualenv .
 source bin/activate
 pip install -r requirements.txt
 export PYTHONPATH=$PWD
-export PATH=$PWD/bin:$PATH
