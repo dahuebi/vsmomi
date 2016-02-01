8c4e51407550277f916b02d027bf3427f52b58be
Andreas Huber <huber@mst.ch>
2016-02-01 10:44:04 +0100

    rc.cmd, improve speed if reentrant

--- rc.cmd
+++ rc.cmd
@@ -1,8 +1,18 @@
+@echo off
 set VENV=env
-py.exe -2 -m virtualenv "%VENV%"
+IF NOT EXIST "%VENV%\.done" (
+    py.exe -2 -m virtualenv "%VENV%" && echo. > "%VENV%\.done"
+)
 
 call "%VENV%\Scripts\activate.bat"
-pip install -r "%~dp0\requirements.txt"
+setlocal
+set "REQ=%~dp0\requirements.txt"
+set "REQ_ENV=%VENV%\requirements.txt"
+fc "%REQ%" "%REQ_ENV%" > nul 2>nul
+IF ERRORLEVEL 1 (
+    pip install -r "%REQ%" && copy /y "%REQ%" "%REQ_ENV%"
+)
+endlocal
 set "PYTHONPATH=%~dp0;%PYTHONPATH%"
 set "PATHEXT=.PY;%PATHEXT%"
 set "PATH=%~dp0\bin;%PATH%"
