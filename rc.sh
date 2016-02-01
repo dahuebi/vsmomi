770a561f6a6b73c7c77608751f695b19b298ead7
Andreas Huber <huber@mst.ch>
2016-02-01 10:59:55 +0100

    rc.sh, improve speed if reentrant

--- rc.sh
+++ rc.sh
@@ -1,12 +1,24 @@
+SAVED_OPTIONS=$(set +o)
+trap "eval \"$SAVED_OPTIONS\"" INT TERM QUIT
+
 WD="`dirname "${BASH_SOURCE[0]}"`"
 cd "$WD"
 WD="`pwd`"
 cd - > /dev/null
 
 VENV=env
-python2 -m virtualenv "$VENV"
+DONE=env/.done
+if ! [ -e "$DONE" ]; then
+    python2 -m virtualenv "$VENV" && \
+        touch "$DONE"
+fi
 
 source "$VENV/bin/activate"
-pip install -r "$WD/requirements.txt"
+REQ="$WD/requirements.txt"
+REQ_ENV="$VENV/requirements.txt"
+if ! diff "$REQ" "$REQ_ENV" > /dev/null; then
+    pip install -r "$WD/requirements.txt" && \
+        cp -f "$REQ" "$REQ_ENV"
+fi
 export PYTHONPATH="$WD:$PYTHONPATH"
 export PATH="$WD/bin:$PATH"
